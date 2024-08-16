70820542941e4857239a6db27ac03a4dc516856c
Inżynier Domu <inzynier.domu@gmail.com>
2024-08-16 23:06:38 +0200

    add libraries

--- src/Config.h
+++ src/Config.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "fonts/Arial14.h"
+
 #include <Arduino.h>
 #include <stdint.h>
 
 namespace Config
 {
     const uint8_t pin_led_ir = 11;
+    const uint8_t sceens_count = 7;
+    const uint8_t *font = Arial14;
+
 } // namespace Config
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,41 @@
 
 #include "Config.h"
 #include "IRremote.h"
+#include "DMD2.h"
+#include "RTClib.h"
 
+#include <SPI.h>
 #include <Arduino.h>
 
 IRrecv irrecv(Config::pin_led_ir);
+
+RTC_DS3231 rtc;
+
+const char *MESSAGE = "abcdefghijklmnopqrstuvwxyz";
+SoftDMD dmd(Config::sceens_count,1);
+DMD_TextBox box(dmd);
 
 void setup() {
   Serial.begin(115200);
   
   irrecv.enableIRIn(); 
+
+  dmd.setBrightness(255);
+  dmd.selectFont(Config::font);
+  dmd.begin();
+  
+   if(!rtc.begin()) {
+        Serial.println("Couldn't find RTC!");
+        Serial.flush();
+        while (1) delay(10);
+    }
+
+    if(rtc.lostPower()) {
+        // this will adjust to the date and time at compilation
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+    //we don't need the 32K Pin, so disable it
+    rtc.disable32K();
 }
 
 void loop() {
