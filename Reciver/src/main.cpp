4800f949c00e23228e66ccc549ab7f1124ef1b02
Inżynier Domu <inzynier.domu@gmail.com>
2024-08-27 23:21:45 +0200

    add reset counter state

--- Reciver/src/main.cpp
+++ Reciver/src/main.cpp
@@ -1,16 +1,19 @@
 
-#include "DMD2.h"
 #include "Config.h"
+#include "DMD2.h"
 
 #include <Arduino.h>
 
-SoftDMD dmd(1,1);
+SoftDMD dmd(1, 2);
 // DMD_TextBox box(dmd);
 
 const int arraySize = 7;
-int16_t receivedData[arraySize];
+// int16_t receivedData[arraySize];
+// const int numOfTokens = 7;
+String tokens[arraySize];
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   dmd.setBrightness(255);
@@ -20,16 +23,32 @@ void setup() {
   // box.print("ID");
 }
 
-void loop() {
-    if (Serial.available() >= arraySize * sizeof(int16_t)) {
-    for (int i = 0; i < arraySize; i++) {
-      Serial.readBytes((char*)&receivedData[i], sizeof(int16_t));      
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+
+    String receivedMessage = Serial.readStringUntil('\n');
+
+    char receivedChars[receivedMessage.length() + 1];
+    receivedMessage.toCharArray(receivedChars, receivedMessage.length() + 1);
+
+    char* token = strtok(receivedChars, ",");
+    int index = 0;
+
+    // Zapisujemy tokeny do tablicy String
+    while (token != NULL && index < arraySize)
+    {
+      tokens[index] = String(token); // Zapisujemy token jako String do tablicy
+      token = strtok(NULL, ",");
+      index++;
     }
 
-    for (int i = 0; i < arraySize; i++)
+    dmd.clearScreen();
+    for (int i = 0; i < 2; i++)
     {
-      String numString = String(receivedData[i]);
-      dmd.drawString(i, 0, numString.c_str(), GRAPHICS_ON, Config::font);
+      String numString = String(tokens[i]);
+      dmd.drawString(0, i * 16, numString.c_str(), GRAPHICS_ON, Config::font);
     }
-    }
+  }
 }
--- Sender/src/main.cpp
+++ Sender/src/main.cpp
@@ -2,19 +2,21 @@
 #include "IRremote.h"
 #include "RTClib.h"
 
-#include <SPI.h>
 #include <Arduino.h>
+#include <SPI.h>
 
 ///< possible device state
 enum class Device_state
 {
   startup,
-  normal_cunting,
+  idle,
+  refersh,
   reset,
-  set_rtc
+  set_rtc,
+  reset_counter
 };
 
-enum command
+enum Button
 {
   BTN_0 = 0,
   BTN_1,
@@ -28,8 +30,47 @@ enum command
   BTN_9,
   BTN_PALY = 10,
   BTN_NEXT = 11,
-  BTN_PREVIOUS = 12
+  BTN_PREVIOUS = 12,
+  BTN_BACK = 13,
+  BTN_ERR = 99
 };
+
+struct Button_command
+{
+  Button button;
+  int16_t command;
+
+  Button_command(Button button_, int16_t command_)
+  : button(button_)
+  , command(command_)
+  {}
+
+  void check_button(int16_t command, Button_command* map)
+  {
+    for (size_t i = 0; i < 14; i++)
+    {
+      if (map[i].command == command)
+      {
+        button = map[i].button;
+      }
+    }
+  }
+};
+
+Button_command buttons[14] = {Button_command(Button::BTN_0, 22),
+                              Button_command(Button::BTN_1, 12),
+                              Button_command(Button::BTN_2, 24),
+                              Button_command(Button::BTN_3, 94),
+                              Button_command(Button::BTN_4, 8),
+                              Button_command(Button::BTN_5, 28),
+                              Button_command(Button::BTN_6, 90),
+                              Button_command(Button::BTN_7, 66),
+                              Button_command(Button::BTN_8, 82),
+                              Button_command(Button::BTN_9, 74),
+                              Button_command(Button::BTN_PALY, 21),
+                              Button_command(Button::BTN_PREVIOUS, 7),
+                              Button_command(Button::BTN_NEXT, 9),
+                              Button_command(Button::BTN_BACK, 67)};
 
 Device_state m_device_state = Device_state::startup; ///< actual device state
 
@@ -40,44 +81,110 @@ int16_t dataToSend[7] = {1234, 5678, 910, 1112, 1314, 1516, 1718};
 const unsigned long m_refresh_time_ms = 30000;
 
 RTC_DS3231 rtc;
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-   IrReceiver.begin(Config::pin_led_ir, ENABLE_LED_FEEDBACK);
-    
-   if(!rtc.begin()) {
-        Serial.println("Couldn't find RTC!");
-        Serial.flush();
-        while (1) delay(10);
-    }
-    if(rtc.lostPower()) {
-        
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    }
-    rtc.disable32K();
+  IrReceiver.begin(Config::pin_led_ir, ENABLE_LED_FEEDBACK);
 
-   m_device_state = Device_state::normal_cunting;
+  if (!rtc.begin())
+  {
+    while (1)
+      delay(10);
+  }
+  if (rtc.lostPower())
+  {
+
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  rtc.disable32K();
+
+  m_device_state = Device_state::refersh;
 }
 
-void button_action()
+Button button_action()
 {
-if (IrReceiver.decodedIRData.protocol == NEC) {
-    auto command = IrReceiver.decodedIRData.command;
+  Button_command command(Button::BTN_ERR, 0);
+  if (IrReceiver.decodedIRData.protocol == NEC)
+  {
+    auto recive_command = IrReceiver.decodedIRData.command;
     IrReceiver.resume();
+    command.check_button(recive_command, buttons);
+    return (command.button);
+  }
+  return (BTN_ERR);
 }
-}
-void loop() {
-  if(IrReceiver.decode()){
-    button_action();
+
+void refresh()
+{
+  String dataString = "";
+
+  for (int i = 0; i < 7; i++)
+  {
+    dataString += String(dataToSend[i]); // Konwersja liczby na String i dodanie do ciągu
+    if (i < 6)
+    {
+      dataString += ","; // Dodanie przecinka i spacji po każdej liczbie, z wyjątkiem ostatniej
+    }
   }
 
-  static unsigned long last_loop_time = 0;
-  unsigned long loop_time = millis();
-  if (loop_time - last_loop_time > m_refresh_time_ms)
+  Serial.println(dataString);
+
+  m_device_state = Device_state::idle;
+}
+
+void idle(Button action)
+{
+  switch (action)
   {
-    for (int i = 0; i < 7; i++) {
-      Serial.write((uint8_t*)&dataToSend[i], sizeof(int16_t));
+    case Button::BTN_BACK:
+      m_device_state = Device_state::reset_counter;
+      break;
+    default:
+    {
+      static unsigned long last_loop_time = 0;
+      unsigned long loop_time = millis();
+      if (loop_time - last_loop_time > m_refresh_time_ms)
+      {
+        m_device_state = Device_state::refersh;
+        last_loop_time = millis();
+      }
     }
-    last_loop_time = millis();
+    break;
+  }
+}
+
+void reset_cunter()
+{
+  dataToSend[0] = 0;
+  // temp
+  m_device_state = Device_state::refersh;
+}
+void loop()
+{
+  Button action = Button::BTN_ERR;
+  if (IrReceiver.decode())
+  {
+    action = button_action();
+  }
+
+  switch (m_device_state)
+  {
+    case Device_state::idle:
+    {
+      idle(action);
+    }
+    break;
+    case Device_state::refersh:
+    {
+      refresh();
+      m_device_state = Device_state::idle;
+    }
+    break;
+    case Device_state::reset_counter:
+      reset_cunter();
+      break;
+    default:
+      break;
   }
 }
