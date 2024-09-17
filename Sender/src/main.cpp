999e63e6ace0f74cb796873734f969f3ecf4de7d
Inżynier Domu <inzynier.domu@gmail.com>
2024-09-17 22:17:52 +0200

    add set counter

--- Sender/src/main.cpp
+++ Sender/src/main.cpp
@@ -174,14 +174,18 @@ void print_set_rtc(simple_time set_time, int8_t filed)
   }
 }
 
-int get_units_digit(int number)
+int power_of_10(int exponent)
 {
-  return number % 10;
+  int result = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    result *= 10;
+  }
+  return result;
 }
-
-int get_tens_digit(int number)
+int get_digit_at_position(int number, int position)
 {
-  return (number / 10) % 10;
+  return (number / power_of_10(position)) % 10;
 }
 void set_rtc(IR_pilot::Button action, bool load = false)
 {
@@ -202,13 +206,13 @@ void set_rtc(IR_pilot::Button action, bool load = false)
     {
       if (digit_to_change == 1)
       {
-        int8_t val = get_tens_digit(new_val.hour);
+        int8_t val = get_digit_at_position(new_val.hour, 1);
         new_val.hour -= (val * 10);
         new_val.hour += static_cast<int>(action) * 10;
       }
       else
       {
-        int8_t val = get_units_digit(new_val.hour);
+        int8_t val = get_digit_at_position(new_val.hour, 0);
         new_val.hour -= val;
         new_val.hour += static_cast<int>(action);
       }
@@ -218,13 +222,13 @@ void set_rtc(IR_pilot::Button action, bool load = false)
     {
       if (digit_to_change == 1)
       {
-        int8_t val = get_tens_digit(new_val.minutes);
+        int8_t val = get_digit_at_position(new_val.minutes, 1);
         new_val.minutes -= (val * 10);
         new_val.minutes += static_cast<int>(action) * 10;
       }
       else
       {
-        int8_t val = get_units_digit(new_val.minutes);
+        int8_t val = get_digit_at_position(new_val.minutes, 0);
         new_val.minutes -= val;
         new_val.minutes += static_cast<int>(action);
       }
@@ -246,13 +250,17 @@ void set_rtc(IR_pilot::Button action, bool load = false)
         if (new_val.check_range())
         {
           save_rtc(new_val);
+          digit_to_change = 1;
           m_device_state = Device_state::refersh;
         }
       }
       break;
       case IR_pilot::Button::BTN_BACK:
+      {
+        digit_to_change = 1;
         m_device_state = Device_state::refersh;
-        break;
+      }
+      break;
       default:
       {
         print_set_rtc(new_val, field_to_change);
@@ -262,32 +270,80 @@ void set_rtc(IR_pilot::Button action, bool load = false)
   }
 }
 
-void set_counter(IR_pilot::Button action)
+bool check_size(int16_t* table)
+{
+  for (size_t i = 0; i < 7; i++)
+  {
+    if (table[i] > 32767)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+void print_set_counter(int16_t* counters, int8_t actual_field)
+{
+  static bool toggle;
+  static unsigned long last_loop_time = 0;
+  unsigned long loop_time = millis();
+  if (loop_time - last_loop_time > Config::blink_time)
+  {
+    blink(counters, actual_field, toggle);
+    toggle = !toggle;
+    last_loop_time = millis();
+  }
+}
+void set_counter(IR_pilot::Button action, bool load = false)
 {
   static int16_t new_days_counter[7] = {10, 11, 12, 13, 14, 15, 16};
   static int8_t actual_field = 0;
-  static int8_t multiply = 1;
+  static int8_t multiply = 0;
+
+  if (load)
+  {
+    for (size_t i = 0; i < 7; i++)
+    {
+      new_days_counter[i] = days_counter[i];
+    }
+  }
 
   if (static_cast<int>(action) < 10)
-  {}
+  {
+    int16_t val = get_digit_at_position(new_days_counter[actual_field], multiply);
+    Serial.println(val);
+    new_days_counter[actual_field] -= (val * power_of_10(multiply));
+    Serial.println(new_days_counter[actual_field]);
+    Serial.println(power_of_10(multiply));
+    new_days_counter[actual_field] += (static_cast<int>(action) * power_of_10(multiply));
+    Serial.println(new_days_counter[actual_field]);
+    if (multiply < 5)
+    {
+      multiply++;
+    }
+  }
   else
   {
     switch (action)
     {
       case IR_pilot::Button::BTN_PLAY:
       {
-        for (size_t i = 0; i < 7; i++)
+        if (check_size(new_days_counter))
         {
-          days_counter[i] = new_days_counter[i];
+          for (size_t i = 0; i < 7; i++)
+          {
+            days_counter[i] = new_days_counter[i];
+          }
+          multiply = 0;
+          m_device_state = Device_state::refersh;
+          refresh();
         }
-        multiply = 1;
-        m_device_state = Device_state::refersh;
-        refresh();
       }
       break;
       case IR_pilot::Button::BTN_BACK:
       {
         m_device_state = Device_state::refersh;
+        multiply = 0;
         refresh();
       }
       break;
@@ -295,88 +351,20 @@ void set_counter(IR_pilot::Button action)
         if (actual_field < 7)
         {
           actual_field++;
-          multiply = 1;
+          multiply = 0;
         }
         break;
       case IR_pilot::Button::BTN_PREVIOUS:
         if (actual_field > 0)
         {
           actual_field--;
-          multiply = 1;
+          multiply = 0;
         }
         break;
       default:
+        print_set_counter(new_days_counter, actual_field);
         break;
     }
-  }
-}
-
-void increment_counter(long days_count)
-{
-  for (int i = 0; i < 6; i++)
-  {
-    days_counter[i] += days_count;
-  }
-}
-
-void check_record()
-{
-  for (int i = 0; i < 6; i++)
-  {
-    auto check_counter = days_counter[i];
-    if (check_counter > days_counter[6])
-    {
-      days_counter[6] = check_counter;
-    }
-  }
-}
-
-void check_day()
-{
-  auto now = get_days_from_start();
-  if (days < now)
-  {
-    auto new_days = get_days_from_start() - days;
-    increment_counter(new_days);
-    check_record();
-    Memory::save_counters(days_counter);
-    days = now;
-  }
-}
-void idle(IR_pilot::Button action)
-{
-  switch (action)
-  {
-    case IR_pilot::Button::BTN_BACK:
-    {
-      m_device_state = Device_state::reset_counter;
-      reset_cunter(IR_pilot::Button::BTN_ERR);
-    }
-    break;
-    case IR_pilot::Button::BTN_PLAY:
-    {
-      m_device_state = Device_state::set_rtc;
-      set_rtc(IR_pilot::Button::BTN_ERR, true);
-    }
-    break;
-    case IR_pilot::Button::BTN_NEXT:
-    {
-      m_device_state = Device_state::set_counter;
-      set_counter(IR_pilot::Button::BTN_ERR);
-    }
-    break;
-    default:
-    {
-      static unsigned long last_loop_time = 0;
-      unsigned long loop_time = millis();
-      if (loop_time - last_loop_time > Config::screen_refresh_time)
-      {
-        check_day();
-        m_device_state = Device_state::refersh;
-        last_loop_time = millis();
-      }
-    }
-    break;
   }
 }
 
@@ -443,6 +431,76 @@ void reset_cunter(IR_pilot::Button action)
     }
   }
 }
+
+void increment_counter(long days_count)
+{
+  for (int i = 0; i < 6; i++)
+  {
+    days_counter[i] += days_count;
+  }
+}
+
+void check_record()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    auto check_counter = days_counter[i];
+    if (check_counter > days_counter[6])
+    {
+      days_counter[6] = check_counter;
+    }
+  }
+}
+
+void check_day()
+{
+  auto now = get_days_from_start();
+  if (days < now)
+  {
+    auto new_days = get_days_from_start() - days;
+    increment_counter(new_days);
+    check_record();
+    Memory::save_counters(days_counter);
+    days = now;
+  }
+}
+void idle(IR_pilot::Button action)
+{
+  switch (action)
+  {
+    case IR_pilot::Button::BTN_BACK:
+    {
+      m_device_state = Device_state::reset_counter;
+      reset_cunter(IR_pilot::Button::BTN_ERR);
+    }
+    break;
+    case IR_pilot::Button::BTN_PLAY:
+    {
+      m_device_state = Device_state::set_rtc;
+      set_rtc(IR_pilot::Button::BTN_ERR, true);
+    }
+    break;
+    case IR_pilot::Button::BTN_NEXT:
+    {
+      m_device_state = Device_state::set_counter;
+      set_counter(IR_pilot::Button::BTN_ERR, true);
+    }
+    break;
+    default:
+    {
+      static unsigned long last_loop_time = 0;
+      unsigned long loop_time = millis();
+      if (loop_time - last_loop_time > Config::screen_refresh_time)
+      {
+        check_day();
+        m_device_state = Device_state::refersh;
+        last_loop_time = millis();
+      }
+    }
+    break;
+  }
+}
+
 void loop()
 {
   IR_pilot::Button action = IR_pilot::Button::BTN_ERR;
