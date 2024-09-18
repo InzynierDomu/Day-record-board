14d8a613c7e86a6ca4aed2131558938f3936fe32
Inżynier Domu <inzynier.domu@gmail.com>
2024-09-18 22:39:07 +0200

    better refresh time

--- Sender/src/Config.h
+++ Sender/src/Config.h
@@ -7,6 +7,6 @@ namespace Config
 {
 const uint8_t pin_led_ir = 11;
 const unsigned long baudrate = 2400;
-const unsigned long screen_refresh_time = 30000; // in ms
-const unsigned long blink_time = 1800; // in ms
+const unsigned long screen_refresh_time = 3600000; // in ms
+const unsigned long blink_time = 1600; // in ms
 } // namespace Config
